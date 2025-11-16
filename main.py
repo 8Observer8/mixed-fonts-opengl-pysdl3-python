18d5d6040bcbe10701b063d419084f4edec41eee
Ivan Enzhaev <8observer8@gmail.com>
2025-11-16 06:54:57 +0400

    Add set_ortho

--- main.py
+++ main.py
@@ -22,23 +22,36 @@ window = None
 mixedText = None
 mixedText2 = None
 
-# Create an orthographic projection matrix and a view matrix
-projMatrix = glm.ortho(-50.0, 50.0, -50.0, 50.0, 10.0, -10.0) # not y-flipped
-# projMatrix = glm.ortho(-50.0, 50.0, 50.0, -50.0, 10.0, -10.0) # y-flipped
-viewMatrix = glm.lookAt(
-    glm.vec3(0, 0, 10), # Camera position
-    glm.vec3(0, 0, 0), # Target position
-    glm.vec3(0, 1, 0)) # Camera up vector
-# Combine them to one projView matrix
-projViewMatrix = projMatrix * viewMatrix
+viewSize = 100.0
+INITIAL_W = 400
+INITIAL_H = 400
+
+projViewMatrix, viewMatrix = None, None
+fontProgram = None
+
+# Set orthographic projection matrix
+def set_ortho(width, height):
+    aspect = (width / height) if (width > 0 and height > 0) else 1.0
+
+    halfHeight = viewSize / 2.0          # fixed vertical size
+    halfWidth  = viewSize * aspect / 2.0 # scale horizontal by aspect
+
+    # Not y-flipped (bottom = -halfHeight, top = +halfHeight)
+    global projMatrix
+    projMatrix = glm.ortho(
+        -halfWidth,   # left
+         halfWidth,   # right
+        -halfHeight,  # bottom
+         halfHeight,  # top
+         10.0,        # near
+        -10.0         # far
+    )
+
+    # projMatrix = glm.ortho(-50.0, 50.0, -50.0, 50.0, 10.0, -10.0) # not y-flipped
+    # projMatrix = glm.ortho(-50.0, 50.0, 50.0, -50.0, 10.0, -10.0) # y-flipped
 
 @sdl3.SDL_AppInit_func
 def SDL_AppInit(appState, argc, argv):
-    global glContext
-    global window
-    global fontProgram
-    global mixedText
-    global mixedText2
 
     if not sdl3.SDL_Init(sdl3.SDL_INIT_VIDEO):
         sdl3.SDL_Log("Couldn't initialize SDL: %s".encode() % sdl3.SDL_GetError())
@@ -47,15 +60,18 @@ def SDL_AppInit(appState, argc, argv):
     sdl3.SDL_GL_SetAttribute(sdl3.SDL_GL_MULTISAMPLEBUFFERS, 1) # Enable MULTISAMPLE
     sdl3.SDL_GL_SetAttribute(sdl3.SDL_GL_MULTISAMPLESAMPLES, 2) # Can be 2, 4, 8 or 16
 
-    title = "PySDL3 PyGLM Pillow OpenGL Python".encode()
-    canvasWidth = 380
-    canvasHeight = 380
-    window = sdl3.SDL_CreateWindow(title, canvasWidth, canvasHeight, sdl3.SDL_WINDOW_OPENGL)
+    title = "Python, OpenGL, PySDL3, PyGLM, Pillow".encode()
+    winW = INITIAL_W
+    winH = INITIAL_H
+    global window
+    window = sdl3.SDL_CreateWindow(title, winW, winH,
+        sdl3.SDL_WINDOW_OPENGL | sdl3.SDL_WINDOW_RESIZABLE)
     if not window:
         sdl3.SDL_Log("Couldn't create a window: %s".encode() % sdl3.SDL_GetError())
         return sdl3.SDL_APP_FAILURE
 
     # Create an OpenGL context
+    global glContext
     glContext = sdl3.SDL_GL_CreateContext(window)
     if not glContext:
         sdl3.SDL_Log("Couldn't create a glContext: %s".encode() % sdl3.SDL_GetError())
@@ -63,13 +79,11 @@ def SDL_AppInit(appState, argc, argv):
 
     sdl3.SDL_GL_SetSwapInterval(1) # Turn on vertical sync
 
-    glViewport(0, 0, canvasWidth, canvasHeight)
+    glViewport(0, 0, winW, winH)
     glEnable(GL_DEPTH_TEST)
-
-    glClearColor(0.75, 0.9, 0.7, 1)
-
     glEnable(GL_BLEND)
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    glClearColor(0.75, 0.9, 0.7, 1)
 
     vertexShaderSource = None
     fragmentShaderSource = None
@@ -78,10 +92,22 @@ def SDL_AppInit(appState, argc, argv):
     with open("./assets/shaders/font.frag") as file:
         fragmentShaderSource = file.read()
 
+    global fontProgram
     fontProgram = compileProgram(
        compileShader(vertexShaderSource, GL_VERTEX_SHADER),
        compileShader(fragmentShaderSource, GL_FRAGMENT_SHADER))
     glUseProgram(fontProgram)
+
+    # Setup projection and view
+    set_ortho(winW, winH)
+    global viewMatrix
+    viewMatrix = glm.lookAt(
+        glm.vec3(0, 0, 10), # Camera position
+        glm.vec3(0, 0, 0), # Target position
+        glm.vec3(0, 1, 0)) # Camera up vector
+
+    global projViewMatrix
+    projViewMatrix = projMatrix * viewMatrix
 
     font = Font(fontProgram, "./assets/fonts/microsoft-yahei/microsoft-yahei.fnt",
         "./assets/fonts/microsoft-yahei/microsoft-yahei.png", False)
@@ -89,20 +115,35 @@ def SDL_AppInit(appState, argc, argv):
     position = glm.vec3(-45, 30, 0)
     size = 250
     color = glm.vec3(0.2, 0.7, 0.5)
+    global mixedText
     mixedText = Text(font, "Hello! Привет!", position, size, color)
 
     position = glm.vec3(-45, 0, 0)
     size = 250
     color = glm.vec3(0.2, 0.7, 0.5)
+    global mixedText2
     mixedText2 = Text(font, "你好! Nǐ hǎo!", position, size, color)
 
     return sdl3.SDL_APP_CONTINUE
 
 @sdl3.SDL_AppEvent_func
 def SDL_AppEvent(appState, event):
-    if sdl3.SDL_DEREFERENCE(event).type == sdl3.SDL_EVENT_QUIT:
+    if not event:
+        return sdl3.SDL_APP_CONTINUE
+
+    ev = sdl3.SDL_DEREFERENCE(event)
+
+    if ev.type == sdl3.SDL_EVENT_QUIT:
         return sdl3.SDL_APP_SUCCESS
-    
+
+    elif ev.type == sdl3.SDL_EVENT_WINDOW_RESIZED:
+        w = ev.window.data1
+        h = ev.window.data2
+        glViewport(0, 0, w, h)
+        set_ortho(w, h)
+        global projViewMatrix, viewMatrix
+        projViewMatrix = projMatrix * viewMatrix
+
     return sdl3.SDL_APP_CONTINUE
 
 @sdl3.SDL_AppIterate_func
@@ -117,8 +158,10 @@ def SDL_AppIterate(appState):
 
 @sdl3.SDL_AppQuit_func
 def SDL_AppQuit(appState, result):
-    global glContext
     global fontProgram
     glDeleteProgram(fontProgram)
+
+    global glContext
     sdl3.SDL_GL_DestroyContext(glContext)
+
     # SDL will clean up the window for us
